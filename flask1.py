cf9f8907caeb3415714779c5688093789d0c00b6
nion8 <nion8@mail.ru>
2016-12-09 10:24:31 +0300

    som 1html

--- flask1.py
+++ flask1.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/user/<username>')
+def xxx(username):
+    return render_template('first.html', name=username)
 
 if __name__ == '__main__':
     app.run()
+
