7f7571bb0761df1e92236b8ad395bc24a724f711
a.b <nion8@mail.ru>
2018-01-05 15:51:01 +0300

    html

--- flask1.py
+++ flask1.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
 
+#
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('first.html', title='Главная страница')
+    #
+    # @app.route('/user/<username>')
+    # def xxx(username):
+    #     return render_template('first.html', name=username)
+    #
 
-@app.route('/user/<username>')
-def xxx(username):
-    return render_template('first.html', name=username)
 
 if __name__ == '__main__':
     app.run()
-    fg
-
-# remdom
+#
+#
+# # remdom
